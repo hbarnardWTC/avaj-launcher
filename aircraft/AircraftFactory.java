debbdd618c03f60f1f3a758122e486292e211bf3
hbarnardWTC <wingarkorin@gmail.com>
2020-08-28 10:43:52 +0200

    Forgot the parsed types XD

--- aircraft/AircraftFactory.java
+++ aircraft/AircraftFactory.java
@@ -14,7 +14,12 @@ package aircraft;
 
 public abstract class AircraftFactory{
 
-	public static Flyable newAircraft(){
+	public static Flyable newAircraft(String type,
+									  String name,
+									  int longitude,
+									  int latitude,
+									  int height)
+	{
 		return null;
 	}
 }
